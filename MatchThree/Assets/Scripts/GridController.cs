02cb741aed373997a37158edbe62261f299b2bc0
Epicnuker129 <zackqgomez@gmail.com>
2023-04-03 17:59:05 -0700

    Attempt

--- MatchThree/Assets/Scripts/GridController.cs
+++ MatchThree/Assets/Scripts/GridController.cs
@@ -1,19 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
-
     [SerializeField]
     private GameObject piecePrefab;
-
     [SerializeField]
     private Vector3 originPosition;
 
+    [Header("Piece Colors")]
+    [SerializeField]
+    private Material pieceOneMaterial;
+    [SerializeField]
+    private Material pieceSecondMaterial;
+    [SerializeField]
+    private Material pieceThirdMaterial;
+    [SerializeField]
+    private Material pieceFourMaterial;
+    [SerializeField]
+    private Material pieceFiveMaterial;
+    [SerializeField]
+    private Material pieceSixMaterial;
     private Piece[,] grid = new Piece[8, 8];
     void Start()
     {
+        System.Random rand = new System.Random();
 
         for (int row = 0; row < grid.GetLength(0); row++)
         {
@@ -22,11 +33,42 @@ public class GridController : MonoBehaviour
                 Vector3 newWorldPosition = new Vector3(originPosition.x + row, originPosition.y, originPosition.z - column);
                 grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
 
-                Instantiate(piecePrefab, grid[row, column].GetPosition(), Quaternion.identity);
+                GameObject gameObject = Instantiate(piecePrefab, grid[row, column].GetPosition(), Quaternion.identity);
+                int theNumber = rand.Next(13, 101);
+                if (theNumber > 30 && theNumber < 45)
+                {
+                    var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+                    gameObjectRenderer.material = pieceOneMaterial;
+                }
+                else if (theNumber >= 45 && theNumber < 60)
+                {
+                    var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+                    gameObjectRenderer.material = pieceFourMaterial;
+                }
+                else if (theNumber >= 60 && theNumber < 85)
+                {
+                    var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+
+                    gameObjectRenderer.material = pieceSecondMaterial;
+                }
+                else if (theNumber >= 85 && theNumber < 101)
+                {
+                    var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+                    gameObjectRenderer.material = pieceThirdMaterial;
+                }
+                else if (theNumber >= 10 && theNumber < 30)
+                {
+                    var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+                    gameObjectRenderer.material = pieceSixMaterial;
+                }
+                else if (theNumber >= 101 && theNumber < 130)
+                {
+                    var gameObjectRenderer = gameObject.GetComponent<Renderer>();
+                    gameObjectRenderer.material = pieceFiveMaterial;
+                }
             }
         }
     }
-
     void Update()
     {
 
--- MatchThree/Assets/Scripts/Pieces.cs
+++ MatchThree/Assets/Scripts/Pieces.cs
@@ -1,26 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum PieceTypes
+{
+    FatRat = 0,
+    JermaRat = 1,
+    Remy = 2,
+    SquishyRat = 3,
+    BiggieCheese = 4,
+    Halo3Rat = 5,
+}
 
 public class Piece
 {
     private Vector3 position;
     private Vector2 gridPosition;
+    private PieceTypes pieceType;
 
     public Piece()
     {
         position = Vector3.zero;
         gridPosition = Vector2.zero;
+        pieceType = PieceTypes.JermaRat;
     }
-
     public Piece(Vector3 position, Vector2 gridPosition)
     {
         this.position = position;
         this.gridPosition = gridPosition;
+        this.pieceType = PieceTypes.SquishyRat;
     }
-
+    public Piece(Vector3 position, Vector2 gridPosition, PieceTypes pieceType)
+    {
+        this.position = position;
+        this.gridPosition = gridPosition;
+        this.pieceType = pieceType;
+    }
     public Vector3 GetPosition()
     {
         return position;
+    }
+    public PieceTypes GetPieceType()
+    {
+        return pieceType;
     }
 }
