61373519cf71363bb65144cda7a6e599dd328f10
Epicnuker129 <zackqgomez@gmail.com>
2023-04-17 14:15:28 -0700

    pushing stuff, not much works the way I want it to
    pushing stuff

--- MatchThree/Assets/Scripts/GridController.cs
+++ MatchThree/Assets/Scripts/GridController.cs
@@ -1,8 +1,10 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridController : MonoBehaviour
 {
+
     [SerializeField]
     private GameObject piecePrefab;
     [SerializeField]
@@ -21,9 +23,32 @@ public class GridController : MonoBehaviour
     private Material pieceFiveMaterial;
     [SerializeField]
     private Material pieceSixMaterial;
-    private Piece[,] grid = new Piece[8, 8];
+
+    public bool pressedDown;
+    public Vector2 pressedDownPosition;
+    public Vector2 pressedUpPosition;
+    public GameObject pressedDownGameObject;
+    public GameObject pressedUpGameObject;
+
+    [Header("UI")]
+    [SerializeField]
+    private GameObject matchesFoundText;
+
+    private Vector2 startMovementPiecePosition;
+    private Vector2 endMovementPiecePosition;
+
+    private bool validMoveInProcess = false;
+
+    private int matchesFound;
+
+    private Piece[,] grid = new Piece[10, 10];
     void Start()
     {
+
+        matchesFound = 0;
+
+        pressedDown = false;
+
         System.Random rand = new System.Random();
 
         for (int row = 0; row < grid.GetLength(0); row++)
@@ -31,46 +56,276 @@ public class GridController : MonoBehaviour
             for (int column = 0; column < grid.GetLength(1); column++)
             {
                 Vector3 newWorldPosition = new Vector3(originPosition.x + row, originPosition.y, originPosition.z - column);
-                grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
-
-                GameObject gameObject = Instantiate(piecePrefab, grid[row, column].GetPosition(), Quaternion.identity);
+                Piece newPiece = new Piece(newWorldPosition, new Vector2(row, column));
+                GameObject gameObject = Instantiate(piecePrefab, newPiece.GetPosition(), Quaternion.identity);
                 int theNumber = rand.Next(13, 101);
                 if (theNumber > 30 && theNumber < 45)
                 {
                     var gameObjectRenderer = gameObject.GetComponent<Renderer>();
                     gameObjectRenderer.material = pieceOneMaterial;
+
+                    newPiece.SetPieceType(PieceTypes.Rat1);
                 }
                 else if (theNumber >= 45 && theNumber < 60)
                 {
                     var gameObjectRenderer = gameObject.GetComponent<Renderer>();
                     gameObjectRenderer.material = pieceFourMaterial;
+
+                    newPiece.SetPieceType(PieceTypes.Rat2);
                 }
                 else if (theNumber >= 60 && theNumber < 85)
                 {
                     var gameObjectRenderer = gameObject.GetComponent<Renderer>();
-
                     gameObjectRenderer.material = pieceSecondMaterial;
+
+                    newPiece.SetPieceType(PieceTypes.Rat3);
                 }
                 else if (theNumber >= 85 && theNumber < 101)
                 {
                     var gameObjectRenderer = gameObject.GetComponent<Renderer>();
                     gameObjectRenderer.material = pieceThirdMaterial;
+
+                    newPiece.SetPieceType(PieceTypes.Rat4);
                 }
                 else if (theNumber >= 10 && theNumber < 30)
                 {
                     var gameObjectRenderer = gameObject.GetComponent<Renderer>();
                     gameObjectRenderer.material = pieceSixMaterial;
+                    newPiece.SetPieceType(PieceTypes.Rat6);
                 }
                 else if (theNumber >= 101 && theNumber < 130)
                 {
                     var gameObjectRenderer = gameObject.GetComponent<Renderer>();
                     gameObjectRenderer.material = pieceFiveMaterial;
+                    newPiece.SetPieceType(PieceTypes.Rat5);
                 }
+
+                PieceController controller = gameObject.GetComponent<PieceController>();
+                controller.SetPiece(newPiece);
+
+                grid[row, column] = newPiece;
             }
         }
     }
-    void Update()
-    {
 
+    private void Update()
+    {
+        if (validMoveInProcess)
+        {
+            Debug.Log("Switching");
+            Vector3 placeHolderPosition = pressedDownGameObject.transform.position;
+            pressedDownGameObject.transform.position = pressedUpGameObject.transform.position;
+            pressedUpGameObject.transform.position = placeHolderPosition;
+            Piece placeHolderPiece = grid[(int)endMovementPiecePosition.x, (int)endMovementPiecePosition.y];
+            grid[(int)endMovementPiecePosition.x, (int)endMovementPiecePosition.y] = grid[(int)startMovementPiecePosition.x, (int)startMovementPiecePosition.y];
+            grid[(int)startMovementPiecePosition.x, (int)startMovementPiecePosition.y] = placeHolderPiece;
+
+            validMoveInProcess = false;
+
+            matchesFound += 1;
+        }
+
+        matchesFoundText.GetComponent<Text>().text = matchesFound.ToString();
+    }
+
+    private Piece GetGridPiece(int row, int column)
+    {
+        Piece foundPiece;
+        try
+        {
+            foundPiece = grid[row, column];
+            if (foundPiece == null || foundPiece.GetDestruction())
+            {
+                return null;
+            }
+
+            return foundPiece;
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        return null;
+    }
+
+    private Piece GetGridPiece(int row, int column, bool isDestroyed)
+    {
+        Piece foundPiece;
+        try
+        {
+            foundPiece = grid[row, column];
+            if (foundPiece == null)
+            {
+                return null;
+            }
+
+            if (!isDestroyed)
+            {
+                return null;
+            }
+
+            return foundPiece;
+        }
+        catch (IndexOutOfRangeException)
+        {
+        }
+
+        return null;
+    }
+
+    public void ValidMove(Vector2 start, Vector2 end)
+    {
+        Debug.Log("validating start (" + start.x + ", " + start.y + ") | end (" + end.x + ", " + end.y + ")");
+        startMovementPiecePosition = start;
+        endMovementPiecePosition = end;
+        bool matchFound = false;
+
+        if (!matchFound)
+        {
+            try
+            {
+                Piece topPiece1 = GetGridPiece((int)end.x, (int)end.y - 1);
+                Piece bottomPiece1 = GetGridPiece((int)end.x, (int)end.y + 1);
+                Debug.Log("Top piece type: " + topPiece1.GetPieceType());
+                Debug.Log("Bottom piece type: " + bottomPiece1.GetPieceType());
+                Piece midPiece1 = GetGridPiece((int)start.x, (int)start.y);
+                Piece toDestroy1 = GetGridPiece((int)end.x, (int)end.y);
+                Debug.Log("Mid piece type: " + midPiece1.GetPieceType());
+                if (topPiece1.GetPieceType() == bottomPiece1.GetPieceType())
+                {
+                    if (topPiece1.GetPieceType() == midPiece1.GetPieceType())
+                    {
+                        matchFound = true;
+                        validMoveInProcess = true;
+                        topPiece1.SetForDestruction();
+                        bottomPiece1.SetForDestruction();
+                        toDestroy1.SetForDestruction();
+                        Debug.Log("MATCH");
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+
+        if (!matchFound)
+        {
+            try
+            {
+                Piece leftPiece = GetGridPiece((int)end.x - 1, (int)end.y);
+                Piece leftLeftPiece = GetGridPiece((int)end.x - 2, (int)end.y);
+                Piece checkPiece1 = GetGridPiece((int)start.x, (int)start.y);
+                if (leftPiece.GetPieceType() == leftLeftPiece.GetPieceType())
+                {
+                    if (leftPiece.GetPieceType() == checkPiece1.GetPieceType())
+                    {
+                        matchFound = true;
+                        validMoveInProcess = true;
+                        Piece toDestroy2 = grid[(int)end.x, (int)end.y];
+
+                        leftPiece.SetForDestruction();
+                        leftLeftPiece.SetForDestruction();
+                        toDestroy2.SetForDestruction();
+                        Debug.Log("MATCH");
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+
+        if (!matchFound)
+        {
+            try
+            {
+                Piece rightPiece = GetGridPiece((int)end.x + 1, (int)end.y);
+                Piece rightRightPiece = GetGridPiece((int)end.x + 2, (int)end.y);
+                Piece checkPiece2 = GetGridPiece((int)start.x, (int)start.y);
+                if (rightPiece.GetPieceType() == rightRightPiece.GetPieceType())
+                {
+                    if (rightPiece.GetPieceType() == checkPiece2.GetPieceType())
+                    {
+                        matchFound = true;
+                        validMoveInProcess = true;
+                        Piece toDestroy2 = GetGridPiece((int)end.x, (int)end.y);
+
+                        rightPiece.SetForDestruction();
+                        rightRightPiece.SetForDestruction();
+                        toDestroy2.SetForDestruction();
+                        Debug.Log("MATCH");
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+
+        if (!matchFound)
+        {
+            try
+            {
+                Piece rightPiece = GetGridPiece((int)end.x + 1, (int)end.y);
+                Piece leftPiece = GetGridPiece((int)end.x - 1, (int)end.y);
+                Piece checkPiece3 = GetGridPiece((int)start.x, (int)start.y);
+                if (rightPiece.GetPieceType() == leftPiece.GetPieceType())
+                {
+                    if (rightPiece.GetPieceType() == checkPiece3.GetPieceType())
+                    {
+                        matchFound = true;
+                        validMoveInProcess = true;
+                        Piece toDestroy2 = GetGridPiece((int)end.x, (int)end.y);
+
+                        rightPiece.SetForDestruction();
+                        leftPiece.SetForDestruction();
+                        toDestroy2.SetForDestruction();
+                        Debug.Log("MATCH");
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+
+        if (!matchFound)
+        {
+            try
+            {
+                Piece abovePiece = GetGridPiece((int)end.x, (int)end.y + 1);
+                Piece aboveAbovePiece = GetGridPiece((int)end.x, (int)end.y + 2);
+                Piece checkPiece4 = GetGridPiece((int)start.x, (int)start.y);
+                if (abovePiece.GetPieceType() == aboveAbovePiece.GetPieceType())
+                {
+                    if (abovePiece.GetPieceType() == checkPiece4.GetPieceType())
+                    {
+                        matchFound = true;
+                        validMoveInProcess = true;
+                        Piece toDestroy2 = GetGridPiece((int)end.x, (int)end.y);
+
+                        abovePiece.SetForDestruction();
+                        aboveAbovePiece.SetForDestruction();
+                        toDestroy2.SetForDestruction();
+                        Debug.Log("MATCH");
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+            }
+        }
+
+        Debug.Log("bad move");
+    }
+    public bool IsDestroyed(Vector2 gridPosition)
+    {
+        Piece piece = GetGridPiece((int)gridPosition.x, (int)gridPosition.y, true);
+        if (piece != null)
+        {
+            return piece.GetDestruction();
+        }
+        return false;
     }
 }
---
+++ MatchThree/Assets/Scripts/Piece.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public enum PieceTypes
+{
+    Rat = 0,
+    Rat1 = 1,
+    Rat2 = 2,
+    Rat3 = 3,
+    Rat4 = 4,
+    Rat5 = 5,
+    Rat6 = 6
+}
+
+public class Piece
+{
+    private Vector3 position;
+    private Vector2 gridPosition;
+    private PieceTypes pieceType;
+    private bool setForDestruction;
+    public Piece()
+    {
+        position = Vector3.zero;
+        gridPosition = Vector2.zero;
+        pieceType = PieceTypes.Rat;
+        setForDestruction = false;
+    }
+    public Piece(Vector3 position, Vector2 gridPosition)
+    {
+        this.position = position;
+        this.gridPosition = gridPosition;
+        this.pieceType = PieceTypes.Rat3;
+        this.setForDestruction = false;
+    }
+    public Piece(Vector3 position, Vector2 gridPosition, PieceTypes pieceType)
+    {
+        this.position = position;
+        this.gridPosition = gridPosition;
+        this.pieceType = pieceType;
+        this.setForDestruction = false;
+    }
+    public void SetForDestruction()
+    {
+        this.setForDestruction = true;
+    }
+    public void SetPieceType(PieceTypes pieceType)
+    {
+        this.pieceType = pieceType;
+    }
+    public void SetGridPosition(Vector2 position)
+    {
+        this.gridPosition = position;
+    }
+    public Vector3 GetPosition()
+    {
+        return position;
+    }
+    public Vector3 GetGridPosition()
+    {
+        return gridPosition;
+    }
+    public PieceTypes GetPieceType()
+    {
+        return pieceType;
+    }
+    public bool GetDestruction()
+    {
+        return setForDestruction;
+    }
+}
